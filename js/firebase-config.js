a812c008d8f1df172baf753aaae0a1f31131c79a
39839 <yairbendor2@gmail.com>
2025-10-28 16:43:37 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,10 @@
 // Montgomery Medical Clinic blog Firebase project configuration.
 // This powers restricted staff sign-in and live blog post delivery.
 window.firebaseConfig = {
-    apiKey: "AIzaSyApTW8VY94FYUzqqliUkaqwJQI9742_5b4",
+    apiKey: "X",
     authDomain: "mmcblog-6573f.firebaseapp.com",
-    projectId: "mmcblog-6573f",
-    storageBucket: "mmcblog-6573f.firebasestorage.app",
-    messagingSenderId: "973375448955",
-    appId: "1:973375448955:web:b419564b9ead8121703fde"
+    projectId: "X",
+    storageBucket: "X",
+    messagingSenderId: "X",
+    appId: "X"
 };
